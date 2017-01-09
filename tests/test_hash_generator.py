8f85d08509234bcf63e80d2984c3206cb6aa796d
Andrei Sura <sura.andrei@gmail.com>
2017-01-09 10:12:21 -0500

    Save preliminary tests

---
+++ tests/test_hash_generator.py
@@ -0,0 +1,64 @@
+"""
+Goal: implement tests for hash_generator.py class
+
+@authors:
+    Andrei Sura <sura.andrei@gmail.com>
+"""
+# flake8: noqa
+import os
+import unittest
+import pandas as pd
+# import pandas.util.testing as tm
+# from mock import patch
+from io import StringIO
+from onefl.hash_generator import HashGenerator
+from onefl import utils
+from onefl import logutils
+
+DELIMITER = '\t'
+logger = logutils.get_a_logger(__file__)
+
+
+class TestHashGenerator(unittest.TestCase):
+
+    def setUp(self):
+        HashGenerator.configure_logger(logger)
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_generate(self):
+        """ Verify that we produce hashes properly """
+        inputdir = 'tests/data_in'
+        outputdir = 'tests/data_out'
+        result = HashGenerator.generate(inputdir, outputdir)
+        self.assertTrue(result)
+
+        # read the reference frame
+        df_expected = utils.frame_from_file(
+            os.path.join('tests/data_in', 'phi_hashes.csv'),
+            delimiter=DELIMITER)
+
+#         file = StringIO(u"""
+# patid| sha_rule_1| sha_rule_2
+# 01 | 1 | 1
+# 02 | 1 | 1
+# 03 | 2 | 1
+# 04 | 2 | 1
+# """)
+#         df_expected = pd.read_csv(file, sep='|',
+#                                   dtype=object,
+#                                   skipinitialspace=True,
+#                                   skip_blank_lines=True)
+
+        # read the frame produced by calling `generate()`
+        df_actual = utils.frame_from_file(
+            os.path.join('tests/data_out', 'phi_hashes.csv'),
+            delimiter=DELIMITER)
+
+        tm.assert_frame_equal(df_expected, df_actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
