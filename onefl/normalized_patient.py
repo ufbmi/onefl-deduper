228cf357e6d7dee1fa9da51215ed52c02cba3988
Andrei Sura <sura.andrei@gmail.com>
2017-01-09 19:06:49 -0500

    update `NormalizedPatient.__init__()` to support kwargs

--- onefl/normalized_patient.py
+++ onefl/normalized_patient.py
@@ -9,12 +9,6 @@ from onefl import utils
 
 
 class NormalizedPatient():
-    log = None
-
-    @classmethod
-    def configure_logger(cls, logger):
-        cls.log = logger
-
     """
     Helper class used to normalize the strings by
     removing punctuation and transforming to lowercase.
@@ -23,23 +17,30 @@ class NormalizedPatient():
 
         Called by :meth:`utils.prepare_for_hashing`
     """
-    def __init__(self, pat):
-        self.id = pat.id
 
-        self.pat_gender = utils.prepare_for_hashing(
-            pat.pat_gender
-        )
-        self.pat_birth_date = utils.format_date_as_string(
-            pat.pat_birth_date, utils.FORMAT_DATABASE_DATE
-        )
+    log = None
+
+    @classmethod
+    def configure_logger(cls, logger):
+        cls.log = logger
+
+    def __init__(self,  *args, **kwargs):
+        self.patid = kwargs.get('patid')
+
         self.pat_first_name = utils.prepare_for_hashing(
-            pat.pat_first_name
+            kwargs.get('pat_first_name')
         )
         self.pat_last_name = utils.prepare_for_hashing(
-            pat.pat_last_name
+            kwargs.get('pat_last_name')
+        )
+        self.pat_birth_date = utils.format_date_as_string(
+            kwargs.get('pat_birth_date'), utils.FORMAT_DATABASE_DATE
+        )
+        self.pat_gender = utils.prepare_for_hashing(
+            kwargs.get('pat_gender')
         )
         self.pat_race = utils.prepare_for_hashing(
-            pat.pat_race
+            kwargs.get('pat_race')
         )
 
     def has_all_data(self, required_attributes):
@@ -50,14 +51,16 @@ class NormalizedPatient():
         # TODO: should we check for minimum length of each string?
         for attr in required_attributes:
             if not getattr(self, attr, None):
-                self.log.debug("--> Missing value for attr [{}]".format(attr))
+                NormalizedPatient.log.warning(
+                    "--> Missing value for attr [{}]".format(attr))
                 return False
         return True
 
     def __repr__(self):
         return "NormalizedPatient " \
-            "<pat_gender: {0.pat_gender}, " \
-            "pat_birth_date: {0.pat_birth_date}, " \
+            "<patid: {0.patid}, " \
             "pat_first_name: {0.pat_first_name}, " \
             "pat_last_name: {0.pat_last_name}, " \
+            "pat_birth_date: {0.pat_birth_date}, " \
+            "pat_gender: {0.pat_gender}, " \
             "pat_race: {0.pat_race}>".format(self)
